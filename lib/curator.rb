ae7fe988b48c9e25237bab48b634247d336afa27
KaraJH <karas.jones@yahoo.com>
2022-11-11 20:30:36 -0700

    Creates methods for curator

--- lib/curator.rb
+++ lib/curator.rb
@@ -20,4 +20,36 @@ class Curator
     end
     found_artist = found.pop
   end
+
+  def find_photograph_by_artist_id(id)
+    found = []
+     @photographs.find_all do |photograph|
+      found << photograph.id.include?(id)
+    end
+  end
+
+  def photo_by_artist
+    pairs = {}
+    @artists.each_with_index do |artist, index|
+      pics = @photographs.find_all do |photograph|
+        if artist.id == photograph.artist_id
+          photos = []
+          photos << photograph.artist_id
+        end
+      end
+      pairs[artist] = pics
+    end
+    pairs
+  end
+
+  def artists_with_multiple_photographs
+    artist_name =[]
+    photo_by_artist.each do |artist|
+      artist.flatten
+        if photo_by_artist[artist[0]].length >= 2
+        artist_name << artist[0].name
+      end
+    end
+    artist_name
+  end
 end
