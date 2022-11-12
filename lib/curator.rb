f73bd2f043d95c4ed46669e7abf3af4cb3ad9f2e
KaraJH <karas.jones@yahoo.com>
2022-11-11 18:28:57 -0700

    Creates method for find artist by id

--- lib/curator.rb
+++ lib/curator.rb
@@ -13,4 +13,11 @@ class Curator
   def add_artist(artist_obj)
     @artists << artist_obj
   end
+
+  def find_artist_by_id(id)
+    found = @artists.find_all do |artist|
+     artist.id.include?(id)
+    end
+    found_artist = found.pop
+  end
 end
