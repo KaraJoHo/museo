91ca5bfac8e55535a19d6d195d62a362bf63d3c9
KaraJH <karas.jones@yahoo.com>
2022-11-11 17:52:49 -0700

    Creates add artist method and adds to artists array

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,2 +1,16 @@
 class Curator
+  attr_reader :photographs, :artists
+
+  def initialize
+    @photographs = []
+    @artists = []
+  end
+
+  def add_photograph(photo_obj)
+    @photographs << photo_obj
+  end
+
+  def add_artist(artist_obj)
+    @artists << artist_obj
+  end
 end
